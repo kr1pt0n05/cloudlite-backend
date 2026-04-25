e87c321a223f9f2763cd7cf93959895a967c4873
kr1pt0n05 <sandrolipinski@outlook.com>
2026-04-25 12:30:30 +0200

    Initial commit

---
+++ src/main/java/de/lind3/CloudLite/CloudLiteApplication.java
@@ -0,0 +1,13 @@
+package de.lind3.CloudLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lind3/CloudLite/CloudLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lind3.CloudLite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
